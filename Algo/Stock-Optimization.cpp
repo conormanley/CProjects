fd5053108a1e1040cb6b320855e09ceb15e3d474
conormanley <conor.manley94@gmail.com>
2020-09-10 15:12:03 -0400

    idk

---
+++ Algo/Stock-Optimization.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <windows.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <thread>
+
+using namespace std;
+
+in main (void) {
+  string inputcosts;
+
+}
--- GPIOClass.h
+++ GPIOClass.h
@@ -16,7 +16,7 @@ public:
   int export_gpio(); // export GPIO
   int unexport_gpio(); // unexport GPIO
   int setdir_gpio(string dir); // Set GPIO Direction
-  int setval_gpio(string dir); // Set GPIO Value (output pins)
+  int setval_gpio(string val); // Set GPIO Value (output pins)
   int getval_gpio(string& val); // Get GPIO Value (input/ output pins)
   string get_gpionum(); // return the GPIO number associated with the instance of an object
 private:
